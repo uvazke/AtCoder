51621c67d788e9e0c81a73dbc0ee8fcfaf248ba4
Kazunori Kihara <kazu.verb.1123@gmail.com>
2019-01-20 15:41:26 +0900

    D prob. is done

--- Others/AISing/D.cpp
+++ Others/AISing/D.cpp
@@ -7,59 +7,64 @@
 #include <iterator>
 
 using namespace std;
-int main(void){
-	long N,Q,A[100000],X[100000],sum=0,ans;
-	vector<long> v;
-	int k=0;
-	int inik = k;
-	cin >> N >> Q;
-	for(int i=0; i<N; i++){
-		cin >> A[i];
-		v.push_back(A[i]);
-		if(i >= N/2)sum += A[i];
+long A[100000];
+long sumO[100000],sumE[100000],sum[100000];
+//TODO two way search
+int solve(long X, int N){
+	int ub = N+1,lb = 1,t;
+	t = (ub + lb)/2;
+	while(ub - lb >1){
+		if(abs(A[N-t]-X) > abs(A[N-(t+1)/2]-X)){
+			ub = t;
+		}else{
+			lb = t;
+		}
+		t = (ub+lb)/2;
 	}
+	// for(int i=1;i<N+1;i++){
+	// 	if(abs(A[N-i]-X) > abs(A[N-(i+1)/2]-X)) return i-1;
+	// }
+	return t;
+}
 
-	for(int i=0; i<Q; i++){
+void calcSum(int N){
+	sumE[0] = A[0];
+	sumO[0] = 0;
+	sum[0] = A[0];
+	for(int i=1;i<N;i++){
+		sum[i] = sum[i-1]+A[i];
+		sumE[i] = sumE[i-1] + (i%2 == 0 ? A[i] : 0);
+		sumO[i] = sumO[i-1] + (i%2 == 1 ? A[i] : 0); 
+	}
+}
+
+int main(void){
+	int N,Q;
+	long X[100000];
+	cin >> N >> Q;
+	for(int i=0;i < N;i++){
+		cin >> A[i];
+	}
+	calcSum(N);
+
+	for(int i=0;i < Q;i++){
 		cin >> X[i];
 	}
-
-	
-	for(int i=0; i<Q; i++){
-		k = inik;
-		while(A[k] < X[i]){
-			k++;
-			if(k>=N)break;
-		}
-		if(k>0)k--;
-		inik = k;
-		
-		if(X[i] <= A[N/2]/2){
-			ans = sum;
+	for(int i=0;i < Q;i++){
+		int a = solve(X[i],N);
+		long sum1 = sum[N-1] - sum[N -(a+1)/2 -1];
+		long sum2;
+		if(N!=a){
+			if(N%2 == 0)sum2 = sumO[N - a - 1];
+			else sum2 = sumE[N - a - 1];
 		}else{
-			vector<long> v2;
-			copy(v.begin(), v.end(), back_inserter(v2) );
-			for(int j=0;j<(N+1)/2;j++){
-				ans += v2.back();
-				v2.pop_back();
-				if(!v2.empty()){
-					if(k < v2.size()-1){
-						if(abs(v2[k] - X[i]) <= abs(v2[k+1] - X[i])){
-							v2.erase(v2.begin() + k);
-							if(k>0)k -= 1;
-						}else{
-							v2.erase(v2.begin() + k+1);
-						}
-					}else{
-						v2.pop_back();
-						if(k>0)k-=1;
-					}
-				}
-			}
-			
+			sum2 = 0;
 		}
-		cout << ans << endl;
-		ans = 0;
+		// for(int j=N -((a+1)/2)*2 -1;j>=0;j-=2){
+		// 	sum2 += A[j];
+		// }
+		cout << sum1 + sum2 << endl;
 	}
-
+	
 	return 0;
 }
