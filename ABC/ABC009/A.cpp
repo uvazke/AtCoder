9006c067bba6e5f7bb7350366593d0a41cda4b0e
Kazunori Kihara <kazu.verb.1123@gmail.com>
2019-06-09 14:57:10 +0900

    Revert "add some solvers"
    This reverts commit db064efa08c5226d36613cfd5c78213bb8789fa0.

--- ABC/ABC009/A.cpp
+++ ABC/ABC009/A.cpp
@@ -3,8 +3,5 @@
 using namespace std;
 
 int main(){
-	int N;
-	cin >> N;
-	cout << (N+1)/2 << endl; 
 	return 0;
 }
--- ABC/ABC128/A.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-int main(){
-	int A,P;
-	cin >> A >> P;
-	cout << (3*A + P)/2 << endl;
-	return 0;
-}
